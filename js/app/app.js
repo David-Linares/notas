216669216adec3a05509c7a1990bf8076b4eae66
David Linares <dlinares@grupodot.com>
2015-08-14 17:00:57 -0500

    Creación del login y del registro de la página

--- js/app/app.js
+++ js/app/app.js
@@ -6,7 +6,8 @@ var app = angular
                 .config(function($routeProvider){
                     $routeProvider
                     	.when('/', {templateUrl: 'templates/login.html', controller: 'loginController'})
-                    	.when('/principal', {templateUrl: 'templates/principal.html', controller: 'principalController'})
+                        .when('/principal', {templateUrl: 'templates/principal.html', controller: 'principalController'})
+                    	.when('/registro', {templateUrl: 'templates/registro.html', controller: 'loginController'})
                         .otherwise({redirectTo: '/'});
                 })
                 .constant('firebaseUrl', 'https://notas.firebaseio.com/')
--- js/app/controllers/controllers.js
+++ js/app/controllers/controllers.js
@@ -12,7 +12,29 @@ app.controller('loginController', ['$scope', 'loginService', '$cookies', '$locat
 		})
 	}
 
+	$scope.registro = function(){
+		console.log("ok");
+		loginService.registrar($scope.nuevoUsuario).then(function(data){
+			console.log(data);
+			$location.path('/principal')
+		}, function(error){
+			console.log(error);
+		})
+	}
+
 }])
+
+app.controller('headerController', ['$scope', 'loginService', '$cookies', '$location' ,function($scope, loginService, $cookies, $location){
+	$scope.logueado = loginService.logueado;
+	$scope.user = loginService.user;
+	console.log($scope.user);
+	$scope.logout = function(){
+		loginService.cerrarSesion();
+		$location.path("/");
+	}
+
+}])
+
 
 app.controller('principalController', ['$scope', 'loginService', '$cookies', '$location' ,function($scope, loginService, $cookies, $location){
 
